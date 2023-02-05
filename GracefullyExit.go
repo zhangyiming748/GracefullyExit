073b913a9449ab280f9c7544fc654b63e1734318
zen <zhangyiming748@gmail.com>
2023-02-05 23:34:38 +0800

    尝试代码

--- GracefullyExit.go
+++ GracefullyExit.go
@@ -7,7 +7,7 @@ import (
 	"syscall"
 )
 
-func ExitAfterRun(try, catch, finally func()) {
+func ExitAfterRun(last func()) {
 	c := make(chan os.Signal)
 	// 监听信号
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
@@ -16,8 +16,7 @@ func ExitAfterRun(try, catch, finally func()) {
 			switch s { // 终端控制进程结束(终端连接断开)|用户发送INTR字符(Ctrl+C)触发|结束程序
 			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM:
 				log.Debug.Println("退出:", s)
-				catch()
-				finally()
+				last()
 			case syscall.SIGUSR1:
 				log.Debug.Println("usr1", s)
 			case syscall.SIGUSR2:
@@ -27,6 +26,4 @@ func ExitAfterRun(try, catch, finally func()) {
 			}
 		}
 	}()
-	log.Debug.Println("启动了程序")
-	try()
 }
